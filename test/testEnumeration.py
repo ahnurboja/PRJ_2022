a9ff9ae2b3cf8091b81668e0cabf36a9d7be79ab
Adrien Nurboja <ahnurboja@pop-os.localdomain>
2022-02-21 10:29:04 +0000

    Test when T clash

--- test/testEnumeration.py
+++ test/testEnumeration.py
@@ -25,11 +25,21 @@ class TestClash(unittest.TestCase):
         enumerate = EnumerationAlgorithm([i1,i2,i3],[m1,m2])
         assert not enumerate.clash(m1, m2)
 
-    # def test_when_only_T_clash(self):
-    #     '''
-    #         Test that clash(m1, m2) returns false when m1 and m2 overlap time
-    #         but share no attendees.
-    #     '''
+    def test_when_only_T_clash(self):
+        '''
+            Test that clash(m1, m2) returns false when m1 and m2 overlap time
+            but share no attendees.
+        '''
+
+        i1 = Individual("i1")
+        i2 = Individual("i2")
+        i3 = Individual("i3")
+
+        m1 = Meeting([i1,i2], i1, 2, 1.0, datetime(2021,12,10,18), datetime(2021,12,10,9))
+        m2 = Meeting([i3], i2, 2, 1.0, datetime(2021,12,10,18), datetime(2021,12,10,10))
+
+        enumerate = EnumerationAlgorithm([i1,i2,i3],[m1,m2])
+        assert not enumerate.clash(m1, m2)
     
     # def test_when_clash(self):
     #     '''
