bc633b11a76ef467f1146d27073937b65f94729d
Adrien Nurboja <ahnurboja@pop-os.localdomain>
2022-02-21 10:28:10 +0000

    Test when I clash

--- test/testEnumeration.py
+++ test/testEnumeration.py
@@ -1,24 +1,38 @@
+import sys
+sys.path.append('../src')
+
 import unittest
 
-from algorithms.enumeration import EnumerationAlgorithm
-from algorithms.scheduler import *
+from enumeration import EnumerationAlgorithm
+from scheduler import *
 from datetime import datetime
 
-class TestGetSolution(unittest.TestCase):
-    def test_get_solution(self):
-        """
-        Test that 
-        """
+class TestClash(unittest.TestCase):
 
-        i1 = Individual("i1", [])
-        i2 = Individual("i2", [])
-        i3 = Individual("i3", [])
-        i4 = Individual("i4", [])
-        i5 = Individual("i5", [])
+    def test_when_only_I_clash(self):
+        '''
+            Test that clash(m1, m2) returns false when m1 and m2 share some
+            of the same attendees but no time overlap.
+        '''
 
-        m1 = Meeting([i1,i3,i4,i5], i1, 3, 1.0, datetime(2021,12,10,18),  datetime(2021,12,10,9))
-        m2 = Meeting([i1,i3,i4,i5], i3, 2, 1.0, datetime(2021,12,10,18),  datetime(2021,12,10,11))
+        i1 = Individual("i1")
+        i2 = Individual("i2")
+        i3 = Individual("i3")
 
-        enumerationAlgorithm = EnumerationAlgorithm
-        
-        enumerationAlgorithm
+        m1 = Meeting([i1,i2,i3], i1, 2, 1.0, datetime(2021,12,10,18), datetime(2021,12,10,9))
+        m2 = Meeting([i1,i3], i2, 2, 1.0, datetime(2021,12,10,18), datetime(2021,12,10,11))
+
+        enumerate = EnumerationAlgorithm([i1,i2,i3],[m1,m2])
+        assert not enumerate.clash(m1, m2)
+
+    # def test_when_only_T_clash(self):
+    #     '''
+    #         Test that clash(m1, m2) returns false when m1 and m2 overlap time
+    #         but share no attendees.
+    #     '''
+    
+    # def test_when_clash(self):
+    #     '''
+    #         Test that clash(m1, m2) returns true when m1 and m2 overlap time
+    #         and share some attendees
+    #     '''
