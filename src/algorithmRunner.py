f6565e1847bb8842405cb20b17b53820b3cf3ff0
ahnurboja <42173085+ahnurboja@users.noreply.github.com>
2022-03-18 14:10:28 +0000

    Algorithm runner (#2)
    * Output results of each algorithm
    
    * Plot graphs to show efficiency and performance
    
    Co-authored-by: Adrien Nurboja <ahnurboja@pop-os.localdomain>

---
+++ src/algorithmRunner.py
@@ -0,0 +1,78 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from randomAlgorithm import RandomAlgorithm
+from enumeration import EnumerationAlgorithm
+
+class AlgorithmRunner:
+
+    solutions = []
+    efficiencies = []
+    performances = []
+
+    def __init__(self, I, M):
+        randomAlgorithm = RandomAlgorithm(I, M)
+        enumerationAlgorithm = EnumerationAlgorithm(I,M)
+
+        self.totalW = 0
+        for m in M:
+            self.totalW += m.w
+
+        self.algorithms = [
+            randomAlgorithm,
+            enumerationAlgorithm,
+        ]
+
+    def runAlgorithms(self):
+        for algorithm in self.algorithms:
+            (S, P) = algorithm.run()
+            self.solutions.append(S)
+            self.performances.append(P)
+        self.calculateEfficiencies()
+    
+    def calculateEfficiencies(self):
+        for S in self.solutions:
+            weights = 0
+            for m in S:
+                weights += m.w
+            self.efficiencies.append(weights/self.totalW)
+
+    def createGraphs(self):
+        names = ["Random", "Enumeration"]
+
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+        axs[0].bar(names, self.efficiencies)
+        axs[0].set_ylim(ymin=0,ymax=1)
+        axs[0].set_ylabel("efficiency")
+        axs[1].bar(names, self.performances)
+        axs[1].set_ylabel("performance")
+        fig.suptitle('Global Efficiency & Performance')
+
+        plt.show()
+            
+
+
+from scheduler import Individual, Meeting
+from datetime import datetime
+
+i1 = Individual("i1")
+i2 = Individual("i2")
+i3 = Individual("i3")
+
+m1 = Meeting('m1', [i1,i2,i3], i1, 2, 4.0, datetime(2021,12,10,17), None)
+m2 = Meeting('m2', [i1,i2,i3], i2, 2, 3.0, datetime(2021,12,10,8), None)
+m3 = Meeting('m3', [i1,i2,i3], i2, 4, 2.0, datetime(2021,12,10,17), None)
+m4 = Meeting('m4', [i1,i2,i3], i2, 3, 1.0, datetime(2021,12,10,13), None)
+
+I = [i1,i2,i3]
+M = [m1,m2,m3,m4]
+runner = AlgorithmRunner(I, M)
+runner.runAlgorithms()
+
+# for i in range(len(runner.algorithms)):
+#     print('\n\n\nAlgorithm ', runner.algorithms[i], ':')
+#     print('\nSolution:\n', runner.solutions[i])
+#     print('\nEfficiency: ', runner.efficiencies[i])
+#     print('\nPerformances: ', runner.performances[i])
+
+runner.createGraphs()
--- src/scheduler.py
+++ src/scheduler.py
@@ -39,20 +39,3 @@ class Meeting:
     
     def __hash__(self):
         return id(self)
-
-# # Example:
-# i1 = Individual("i1", [])
-# i2 = Individual("i2", [])
-# i3 = Individual("i3", [])
-# i4 = Individual("i4", [])
-# i5 = Individual("i5", [])
-
-# # Scheduled Meetings:
-# m1 = Meeting([i1,i3,i4,i5], i1, 1, 1.0, datetime.datetime(2021,12,10,18),  datetime.datetime(2021,12,10,9))
-# m2 = Meeting([i1,i3,i4,i5], i3, 1, 1.0, datetime.datetime(2021,12,10,18),  datetime.datetime(2021,12,10,11))
-# m3 = Meeting([i4,i5], i4, 2, 1.0, datetime.datetime(2021,12,10,18),  datetime.datetime(2021,12,10,16))
-
-# # Unscheduled Meetings:
-# m4 = Meeting([i1,i3,i5], i1, 1, 1.0, datetime.datetime(2021,12,10,18), None)
-# m5 = Meeting([i1,i3,i4,i5], i5, 2, 1.0, datetime.datetime(2021,12,10,18), None)
-# m6 = Meeting([i1,i2,i4], i2, 1, 1.0, datetime.datetime(2021,12,10,18), None)
